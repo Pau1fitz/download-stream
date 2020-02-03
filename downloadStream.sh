93ceeb365177f4627b21b77d5a1f4a74e741341d
Paul Fitzgerald <paulfitz99@gmail.com>
2020-02-03 11:37:51 +0000

    Update downloadStream.sh

--- downloadStream.sh
+++ downloadStream.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-LINK=(
-  'https://bxjlp.mcloud.to/26/2b8bd5748b8df2a0/75075bf5e8f8a9ac95dd6f4b55a2eb695c86e4fee5c5f7910697bb6723a89bb9718e30e74e69122680a911ed900994bcd3c9bca379fa7bd2ce1fe7e602bb0cb53a8feebd8e112be60cd1a430f6aa4cb8ed5ac43baf27a6f7949107b57d7040d681a292660e3afdf4979a7d458d227411/hls/480/480' # replace this with your url 
-)
-
 # movie/tv show name
 NAME=$1
+
+LINK_URL=$2
+
+LINK=($LINK_URL)
 
 function is_ffmpgeg_required {
   if brew ls --versions ffmpeg > /dev/null; 
